b4ad99a59af0259c03b5d00d2b8da4e6c00ae29e
furuhama <furuhama@users.noreply.github.com>
2018-08-19 18:32:33 +0900

    Create simple svg pattern

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,17 @@ extern crate svg_monsters;
 
 mod svg_pixel;
 
-use svg_pixel::SvgGenerator;
+use svg_pixel::{Canvas, SvgGenerator};
 
 fn main() {
-    let content = String::from("hoge\n");
-    let _ = SvgGenerator::save(content, "./hoge.txt");
+    let dot_pattern = vec![
+        vec![1, 0, 0, 0],
+        vec![0, 1, 0, 0],
+        vec![0, 0, 1, 0],
+        vec![0, 0, 0, 1]
+    ];
+    let svg_content = Canvas::from_vec(dot_pattern).to_svg();
+    let _ = SvgGenerator::save(svg_content, "svg/simple_line.svg");
 
     println!("SVG Monsters");
 }
