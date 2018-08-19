0d0a928f1bb9c271376e8b22e7e87f47ba81b35b
furuhama <furuhama@users.noreply.github.com>
2018-08-19 18:17:58 +0900

    Try to use SvgGenerator::save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate svg_monsters;
+
+mod svg_pixel;
+
 fn main() {
+    let content = String::from("hoge\n");
+    let _ = svg_pixel::svg_generator::SvgGenerator::save(content, "./hoge.txt");
+
     println!("SVG Monsters");
 }
