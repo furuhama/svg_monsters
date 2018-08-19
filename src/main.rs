52d7b4cd62a02bc6741525c6d433f1cad86894af
furuhama <furuhama@users.noreply.github.com>
2018-08-19 20:15:23 +0900

    Call generate_randomly() from main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,21 @@ extern crate svg_monsters;
 
 mod svg_pixel;
 
-use svg_pixel::{Canvas, SvgGenerator};
+use svg_pixel::{Canvas, SvgGenerator, MonsterPattern};
 
 fn main() {
-    let dot_pattern = vec![
-        vec![1, 0, 0, 0],
-        vec![0, 1, 0, 0],
-        vec![0, 0, 1, 0],
-        vec![0, 0, 0, 1]
-    ];
-    let svg_content = Canvas::from_vec(dot_pattern).to_svg();
-    let _ = SvgGenerator::save(svg_content, "svg/simple_line.svg");
+    // let dot_pattern = vec![
+    //     vec![1, 0, 0, 0],
+    //     vec![0, 1, 0, 0],
+    //     vec![0, 0, 1, 0],
+    //     vec![0, 0, 0, 1]
+    // ];
+    // let svg_content = Canvas::from_vec(dot_pattern).to_svg();
+    // let _ = SvgGenerator::save(svg_content, "svg/simple_line.svg");
+
+    let monster_pattern = MonsterPattern::generate_ramdomly();
+    let svg_content = Canvas::from_vec(monster_pattern).to_svg();
+    let _ = SvgGenerator::save(svg_content, "svg/monster_pattern.svg");
 
     println!("SVG Monsters");
 }
--- src/svg_pixel/mod.rs
+++ src/svg_pixel/mod.rs
@@ -7,3 +7,4 @@ mod monster_patterns;
 
 pub use self::basic::Canvas;
 pub use self::svg_generator::SvgGenerator;
+pub use self::monster_patterns::MonsterPattern;
